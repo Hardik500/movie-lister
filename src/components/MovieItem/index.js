a56fd550cf30af43a89a910a5535dd2be5706cc4
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-08-12 16:01:27 +0530

    [refactor]

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -2,7 +2,7 @@ import './style.css'
 import Image from '../Helper/Image';
 
 export default function MovieItem({data, width, height}) {
-    const {title, poster_path} = data;
+    const {title, poster_path, release_date} = data;
 
     const thumbnail = 'https://image.tmdb.org/t/p/w200/' + poster_path
     const src = 'https://image.tmdb.org/t/p/w500/' + poster_path
@@ -12,7 +12,9 @@ export default function MovieItem({data, width, height}) {
             <Image alt={title} src={src} thumbnail={thumbnail} width={width} height={height} />
             <div className="movie-item--overlay"></div>
             <div className="movie-item--overlay-text">
-                <p className="movie-item--title">{title}</p>
+                <p className="movie-item--title movie-item--title-1">{title}</p>
+                <br/>
+                <p className="movie-item--title movie-item--title-2">{release_date.split("-")[0]}</p>
             </div>
         </div>
     )
